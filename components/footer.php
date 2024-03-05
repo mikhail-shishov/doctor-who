56243295c26d1557000c6a19ae3fdd6c9740330e
mksh-su <shishovmike96@gmail.com>
2024-03-05 07:47:13 +0100

    Initial commit

---
+++ components/footer.php
@@ -0,0 +1,28 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <p>Vytvoril Mikhail Shishov &copy; 2023</p>
+                <p><a href="mailto:mikhail.shishov@student.ukf.sk">mikhail.shishov@student.ukf.sk</a></p>
+            </div>
+            <div class="col-3">
+                <p><a href="index.html">Hlavná</a></p>
+                <p><a href="contact.html">Kontakt</a></p>
+            </div>
+            <div class="col-3">
+                <p><a href="about.html">O Doktore</a></p>
+                <p><a href="enemy.html">Nepriateľia</a></p>
+            </div>
+        </div>
+        <p class="copyright">
+            Toto je fan stránka. Práva na seriál patria <a href="https://www.bbc.com/" target="_blank" rel="nofollow noopener">BBC</a>.
+        </p>
+    </div>
+</footer>
+
+<script src="js/simple-lightbox.min.js"></script>
+<script src="js/splide.min.js"></script>
+<script src="js/scripts.js"></script>
+</body>
+
+</html>
---
+++ thank-you.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="sk">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="My sa Vám ozveme." />
+    <meta name="keywords" content="ďakujeme" />
+    <meta name="author" content="Mikhail Shishov" />
+    <meta name="robots" content="noindex">
+
+    <!-- favicon start -->
+    <link href="favicon.ico" rel="icon" />
+    <link href="img/favicon.png" rel="icon" sizes="any" />
+    <!-- favicon end -->
+
+    <link rel="stylesheet" href="css/simple-lightbox.min.css" />
+    <link rel="stylesheet" href="css/splide.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <title>Ozveme sa | Doctor Who</title>
+  </head>
+  <body>
+    <header class="header">
+      <div class="container">
+        <div class="header-wrap">
+          <a href="index.html" class="logo">
+            <img src="img/logo.svg" width="80" height="80" alt="Doctor Who official logo" />
+          </a>
+          <span class="burger"></span>
+          <nav class="header-nav">
+            <a href="index.html">Hlavná</a>
+            <a href="about.html">O Doktore</a>
+            <a href="enemy.html">Nepriateľia</a>
+            <a href="contact.html" class="is-active">Kontakt</a>
+          </nav>
+        </div>
+      </div>
+    </header>
+
+    <main class="main">
+      <div class="contact-sect">
+        <div class="container">
+          <h1 class="h1">Ďakujeme!</h1>
+        </div>
+      </div>
+    </main>
+
+    <footer class="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-6">
+            <p>Vytvoril Mikhail Shishov &copy; 2023</p>
+            <p><a href="mailto:mikhail.shishov@student.ukf.sk">mikhail.shishov@student.ukf.sk</a></p>
+          </div>
+          <div class="col-3">
+            <p><a href="index.html">Hlavná</a></p>
+            <p><a href="contact.html">Kontakt</a></p>
+          </div>
+          <div class="col-3">
+            <p><a href="about.html">O Doktore</a></p>
+            <p><a href="enemy.html">Nepriateľia</a></p>
+          </div>
+        </div>
+        <p class="copyright">
+          Toto je fan stránka. Práva na seriál patria <a href="https://www.bbc.com/" target="_blank" rel="nofollow noopener">BBC</a>.
+        </p>
+      </div>
+    </footer>
+
+    <script src="js/simple-lightbox.min.js"></script>
+    <script src="js/splide.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
+
